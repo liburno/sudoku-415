459c3a0993e862c5f64c0950916d07ac4b1c730e
ago <agostino.alessio@gmail.com>
2019-07-28 10:41:21 +0200

    clean code

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,47 +1,47 @@
 function shuffle(array) {
-    let counter = array.length;
-   while (counter > 0) {
-        let index = Math.floor(Math.random() * counter);
-        counter--;
-        let temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
-    }
-    return array;
+	let counter = array.length;
+	while (counter > 0) {
+		let index = Math.floor(Math.random() * counter);
+		counter--;
+		let temp = array[counter];
+		array[counter] = array[index];
+		array[index] = temp;
+	}
+	return array;
 }
 
 function getstorage(x) {
 	return JSON.parse(window.localStorage.getItem(x));
 }
-function setstorage(x,v) {
+function setstorage(x, v) {
 	if (!v) {
 		window.localStorage.removeItem(x);
 	} else {
-		window.localStorage.setItem(x,JSON.stringify(v));
+		window.localStorage.setItem(x, JSON.stringify(v));
 	}
 }
 
 function sounddes() {
-    return issound?"☐":"♫";
+	return issound ? "☐" : "♫";
 }
 
-function clamp(v,v1,v2) {
-	if (v<v1) return v1;
-	if (v>v2) return v2;
+function clamp(v, v1, v2) {
+	if (v < v1) return v1;
+	if (v > v2) return v2;
 	return v;
 }
 
-function elapsed(d1,d2) {
-	function pad(n,d) {
+function elapsed(d1, d2) {
+	function pad(n, d) {
 		n = n + '';
 		return n.length >= d ? n : new Array(d - n.length + 1).join('0') + n;
 	}
 	var td;
 	if (d2) {
-		td = d2-d1;
-		td=td/1000;
+		td = d2 - d1;
+		td = td / 1000;
 	} else {
-		td=d1;
+		td = d1;
 	}
 	if (!td) return "";
 	var s = Math.round(td % 60);
@@ -49,17 +49,17 @@ function elapsed(d1,d2) {
 	var m = Math.round(td % 60);
 	td = Math.floor(td / 60);
 	var h = td
-	var tm="";
-	if (h) tm=h+"h ";
-	if (m || h) tm=tm+pad(m,2)+'\' '
-	tm=tm+pad(s,2) +'"';
+	var tm = "";
+	if (h) tm = h + "h ";
+	if (m || h) tm = tm + pad(m, 2) + '\' '
+	tm = tm + pad(s, 2) + '"';
 	return tm;
 }
 
-function ligthen(c,v) {
-    var r={};
-    r.r=min(255,max(0,c.r+v*255))
-    r.g=min(255,max(0,c.g+v*255))
-    r.b=min(255,max(0,c.b+v*255))
-    return r;
+function ligthen(c, v) {
+	var r = {};
+	r.r = min(255, max(0, c.r + v * 255))
+	r.g = min(255, max(0, c.g + v * 255))
+	r.b = min(255, max(0, c.b + v * 255))
+	return r;
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,31 @@
-var w,ox,oy;
-var CW,CH;
+var w, ox, oy;
+var CW, CH;
 var sudoku;
 var nine;
-var ui,btreset,btnsound,btnhelp,btnhelp2,btnrestart,btninfo,btk,issound,clicksound;
+var ui, btreset, btnsound, btnhelp, btnrestart, btninfo, issound, clicksound;
 var numbersel;
-var applause,tosse,solver,undosolver;
-var xxlevel=0;
-var ishelped=false;
-var dstart,dend;
+var applause, tosse, solver, undosolver;
+var xxlevel = 0;
+var ishelped = false;
+var dstart, dend;
 
 
 function preload() {
-  applause=loadSound('../songs/umgawa.mp3');
-  tosse=loadSound('../songs/tosse.mp3');
-  clicksound=loadSound("../songs/click.mp3");
- 
+  applause = loadSound('../songs/umgawa.mp3');
+  tosse = loadSound('../songs/tosse.mp3');
+  clicksound = loadSound("../songs/click.mp3");
+
 }
 function mouseClicked() {
-  ui.mousepress(mouseX,mouseY);
+  ui.mousepress(mouseX, mouseY);
 }
 function mousePressed() {
-   sudoku.mousepress(mouseX,mouseY);
+  sudoku.mousepress(mouseX, mouseY);
 }
 
 function mouseMoved() {
   if (!mouseIsPressed) {
-      if (ui) ui.mousemove(mouseX,mouseY);
+    if (ui) ui.mousemove(mouseX, mouseY);
   }
 }
 function keyPressed() {
@@ -34,69 +34,66 @@ function keyPressed() {
 
 
 function recalcpos() {
-  btreset.move(ox+w*10.5,oy,w*3)
-  btnrestart.move(ox+w*9.5,oy+w)
-  btnsound.move(ox+w*10.5,oy+w)
-  btnhelp.move(ox+w*11.5,oy+w)
-  btninfo.move(ox+w*10.5,oy+w*7.5,w*3)
+  btreset.move(ox + w * 10.5, oy, w * 3)
+  btnrestart.move(ox + w * 9.5, oy + w)
+  btnsound.move(ox + w * 10.5, oy + w)
+  btnhelp.move(ox + w * 11.5, oy + w)
+  btninfo.move(ox + w * 10.5, oy + w * 7.5, w * 3)
 
 }
 
 function windowResized() {
   checksize();
-  resizeCanvas(CW,CH);
+  resizeCanvas(CW, CH);
   ui.recalcbuttsize(CW)
   recalcpos();
 }
 
 function checksize() {
-  CW=windowWidth;
-  CH=windowHeight
-  w=CW/13.5
-  if (CH/10<w) w=CH/10;
-  w=floor(w);
-  ox=floor(w*.8);
-  oy=floor(w*.8);  
-  
+  CW = windowWidth;
+  CH = windowHeight
+  w = CW / 13.5
+  if (CH / 10 < w) w = CH / 10;
+  w = floor(w);
+  ox = floor(w * .8);
+  oy = floor(w * .8);
+
 }
 function setup() {
   checksize();
-  var can=createCanvas(CW,CH).parent("canvas");
-  
-  sudoku=new Sudoku();
-  undosolver=[];
-  if (!sudoku.fromLocalStorage())   sudoku.reset();
-  ui=new UI();
-  btk=[];
+  var can = createCanvas(CW, CH).parent("canvas");
 
-
-  btreset=new Button("New",() =>{
-    sudoku.reset() 
-  } );
-  btnrestart=new Help(0,0,() =>{
+  sudoku = new Sudoku();
+  undosolver = [];
+  if (!sudoku.fromLocalStorage()) sudoku.reset();
+  ui = new UI();
+  btreset = new Button("New", () => {
+    sudoku.reset()
+  });
+  btnrestart = new Help(0, 0, () => {
     sudoku.fromLocalStorage();
-  } ,'↺');
-  btninfo=new Button("info",() =>{
-    window.location.hash="#help"
-  } ,0,0);
-  
-  issound=!(!getstorage("sound"))
-  btnsound=new Help(ox+w*9.8,oy+w,()=>{
-    issound=!issound;
-    btnsound.text=sounddes();
-    setstorage("sound",issound);
-  } ,sounddes());
-  btnhelp=new Help(ox+w*11.2,oy+w,()=>{
-    ishelped=!ishelped
-  },"?");
+  }, '↺');
+  btninfo = new Button("info", () => {
+    window.location.hash = "#help"
+  }, 0, 0);
 
-  ui.push([btreset,btnsound,btnhelp,btninfo,btnrestart]);
- 
+  issound = !(!getstorage("sound"))
+  btnsound = new Help(ox + w * 9.8, oy + w, () => {
+    issound = !issound;
+    btnsound.text = sounddes();
+    setstorage("sound", issound);
+  }, sounddes());
+  btnhelp = new Help(ox + w * 11.2, oy + w, () => {
+    ishelped = !ishelped
+  }, "?");
+
+  ui.push([btreset, btnsound, btnhelp, btninfo, btnrestart]);
+
   recalcpos();
   frameRate(10);
 }
 function draw() {
-  background(0,128,0);
+  background(0, 128, 0);
   sudoku.draw();
   ui.draw();
 }
